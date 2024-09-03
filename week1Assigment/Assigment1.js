e02ba13d011bb414533437b1f816b6377b4dbe66
19bcs1715 <19bcs1715@gmail.com>
2024-09-03 18:44:15 +0530

    Assigment 1

---
+++ week1Assigment/Assigment1.js
@@ -0,0 +1,20 @@
+// function sum(a,b) {
+//   return a+b;
+
+
+// }
+// console.log(sum("Rahul",21));
+
+
+function age(voteAge) {
+  if (voteAge >= 18) {
+    return ("He can vote");
+
+
+  } else {
+
+    return ("He can't vote")
+  }
+
+}
+console.log(age(21));
